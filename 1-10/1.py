7bdce55e7fd9849d8f97a18da3a3f7ab5de462ae
slamail76 <slamail76@gmail.com>
2022-05-08 20:59:54 +0200

    Initial commit

---
+++ 1-10/1.py
@@ -0,0 +1,10 @@
+#Esercizio numero 1
+
+num = float(input("Inserisci un numero: "))
+
+if num > 0:
+    print(f"Il numero {num} è positivo")
+elif num == 0:
+    print(f"Il numero {num} è = 0")
+else:
+    print((f"Il numero {num} è negativo"))
